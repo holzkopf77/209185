844925d88851c904e40abe0a4024aa81a8d42997
Budyn <daniel.budynski@gmail.com>
2015-03-06 00:48:40 +0100

    boostimplementation

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,11 +1,28 @@
-#include <boost\lambda\lambda.hpp>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
+#include <ctime>
+#include <random>
+#include <boost\timer\timer.hpp>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << "Hi folks.";
-	std::cin.get();
+	using std::cout;
+	using std::endl;
+	using std::cin;
+	using std::mt19937;
+	using std::uniform_int_distribution;
+
+	int* intTable = new int[argc];
+	int seedGen = time(NULL);
+	int Counter = 0;
+	mt19937 randomNumbr(seedGen);
+	uniform_int_distribution<>range(0, argc);
+
+	//Start measure time
+	boost::timer::auto_cpu_timer t;
+	for (Counter; Counter < argc; Counter++)
+	{
+		intTable[Counter] = range(randomNumbr);
+	}
+	cin.get();
 
 }
