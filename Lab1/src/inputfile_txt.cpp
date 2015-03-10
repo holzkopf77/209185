08c1cac9762b5219b5f06fcd90fc9fdd0008a79e
Budyn <daniel.budynski@gmail.com>
2015-03-10 21:49:06 +0100

    Input files class

--- Lab1/src/inputfile_txt.cpp
+++ Lab1/src/inputfile_txt.cpp
@@ -1,3 +1,23 @@
-/*! \brief A new file object class source code */
+/*! A new file object class source code */
 #include "inputfile_txt.h"
+
+InputFiles::InputFiles(){
+
+	const int UNDEF_VALUE = 1;
+	filesNumber = UNDEF_VALUE;
+
+	//TODO: EXCEPTIONS HANDLING
+	std::string TempName = std::tmpnam(nullptr);
+
+	filesNamesTab.push_back(TempName);
+	filesSize.push_back(UNDEF_VALUE);
+}
+
+std::string InputFiles::show_info(){
+	std::cout << "--------" << std::endl;
+	std::cout << filesNumber << std::endl;
+	std::cout << filesNamesTab[0] << std::endl;
+	std::cout << filesSize[0] << std::endl;
+	std::cout << "--------" << std::endl;
+}
 
--- Lab1/src/inputfile_txt.h
+++ Lab1/src/inputfile_txt.h
@@ -1,16 +1,43 @@
-/*! \brief A new file object class header */
+/*! \file inputfile_txt.h
+	\brief A new input files class
+*/
 #ifndef inputfile_txt
 #define inputfile_txt
 
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <cstdio>
+/*! \class InputFiles
 
-class inputfile {	
+	Making an object which contain text files with 
+	generated random integer numbers.
+*/
+class InputFiles {	
 	public:
-		inputfile();
+		//! A default constructor
+		/*!
+			Adding number of files(UNDEF_VALUE = 1);
+			Generating file name;
+			Adding size of file (UNDEF_VALUE = 1);
+
+			Just in case, when program starts without any parameters.
+		*/
+		InputFiles();
+		
+		//! A constructor
+		/*!
+			Adding number of files(1);
+			Generating files names(1);
+		*/
+		InputFiles(int filNr, std::vector<int>filSiz);
+
+		//! Show info about files
+		std::string show_info();
 	private:
-		int FilesNumber;
-		std::string FilesNamesTab;
+		int filesNumber;
+		std::vector<std::string>filesNamesTab;
+		std::vector<int>filesSize;
 };
 #endif
