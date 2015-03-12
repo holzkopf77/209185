6969c888ad98690cb4bf11729f186307ea848f72
Budyn <daniel.budynski@gmail.com>
2015-03-12 10:37:02 +0100

    edited readme

--- Lab1/src/benchmark_frm.cpp
+++ Lab1/src/benchmark_frm.cpp
@@ -56,7 +56,6 @@ void Benchmark::test(InputFiles files){
 	// temp memory container
 	int* tabForData = NULL;
 	int tempValue = 0;
-	int count = 0;
 	std::fstream newFile;
 
 	for (int i = 0; i < files.return_number_files() - FIRST_ARGUMENT; i++){
