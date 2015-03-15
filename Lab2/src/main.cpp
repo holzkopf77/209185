acf6ba33b13df6a7d0920f6d8f448f886d95f988
Budyn <daniel.budynski@gmail.com>
2015-03-15 12:41:01 +0100

    NewFolders

---
+++ Lab2/src/main.cpp
@@ -0,0 +1,8 @@
+/*! \file main.cpp
+	\brief Main function
+*/
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
---
+++ Lab2/src/stack.cpp
@@ -0,0 +1,8 @@
+/*! \file stack.cpp
+	\brief A source code for Stack class.
+*/
+#include "stack.h"
+
+Stack::Stack(int stackSize){
+
+}
